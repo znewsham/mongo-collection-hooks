0b22f1accc8931867de40d13eb199ddd352f47d7
Zack Newsham <zack.newsham@gmail.com>
2024-07-04 17:57:30 -0400

    export hooked cursor options

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { HookedAggregationCursor } from "./hookedAggregationCursor.js";
 export { HookedCollection } from "./hookedCollection.js";
-export { HookedFindCursor } from "./hookedFindCursor.js";
+export { HookedFindCursor, HookedFindCursorOptions } from "./hookedFindCursor.js";
 export {
   Events,
   SkipDocument,
